725089fc23586f129c7b0d75d33941a9442bbec9
Nileshv96 <nileshvaidya1909@gmail.com>
2025-01-30 13:08:08 +0530

    second commit

--- Desktop/FAST_API/Fast3/main.py
+++ Desktop/FAST_API/Fast3/main.py
@@ -1,1 +1,25 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+import crud, schemas, database
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+@router.post("/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    return crud.create_user(db, user)
+
+@router.get("/", response_model=list[schemas.UserResponse])
+def get_users(db: Session = Depends(database.get_db)):
+    return crud.get_users(db)
+
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from curd,cshemas,database
+
+router=APIRouter()
+@router.post("/",response_model=schemas,UserResponse)
+def create_user(user:schemas.UserCreate,db:Session = Depends(database.getdb)):
+    return crud.create_user(db,user)
+
+@router.get("/",response_model )
